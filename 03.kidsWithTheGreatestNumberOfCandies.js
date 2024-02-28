048f0606158ec6290588152d85aab9a008187cc8
triduong <mtworkweb@gmail.com>
2024-02-28 22:09:38 +0700

    1431. Kids With the Greatest Number of Candies

---
+++ 03.kidsWithTheGreatestNumberOfCandies.js
@@ -0,0 +1,22 @@
+// Challenge https://leetcode.com/problems/kids-with-the-greatest-number-of-candies
+
+/**
+ * @param {number[]} candies
+ * @param {number} extraCandies
+ * @return {boolean[]}
+ */
+var kidsWithCandies = function(candies, extraCandies) {
+    const maxCandies = Math.max(...candies)
+
+    const result = []
+
+    for (let i = 0; i < candies.length; i++) {
+        if(candies[i] + extraCandies >= maxCandies) {
+            result.push(true)
+        } else {
+            result.push(false)
+        }
+    }
+
+    return result
+};
